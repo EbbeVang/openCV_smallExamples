d418d71a91e8286f85919d2668f5c46ded1717d6
Ebbe Vang <ev@evang.dk>
2021-03-17 13:52:30 +0100

    added webcam example

---
+++ readWebCam.py
@@ -0,0 +1,13 @@
+import cv2
+
+videoCapture  =cv2.VideoCapture(0)
+
+#set withand height
+videoCapture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+videoCapture.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
+
+while True:
+    succes, img = videoCapture.read()
+    cv2.imshow('video', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
