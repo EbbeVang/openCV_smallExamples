ac15574848a09fb6b5b6c1995843495a9f9f9104
Ebbe Vang <ev@evang.dk>
2021-03-18 11:58:55 +0100

    fixed readWebcam example

--- facialRecognition.py
+++ facialRecognition.py
@@ -5,6 +5,7 @@ videoCapture  =cv2.VideoCapture(0)
 #set withand height
 videoCapture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 videoCapture.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
+videoCapture.set(bright)
 while True:
     succes, img = videoCapture.read()
     cv2.imshow('video', img)
